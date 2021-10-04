715e958e82e4303123bae82eb8eac9672b48ad27
Your Name <you@example.com>
2021-10-03 21:28:13 -0300

    firebase atualizaçao

--- app.js
+++ app.js
@@ -15,13 +15,16 @@ var firebaseConfig = {
 
 function botao() {
 
+
 		let pet = {
-			"nome":document.getElementById("inNome")		.value.toUpperCase().trim(),
-			"idade": document.getElementById("inIdade")		.value.toUpperCase().trim(),
-			"cor": document.getElementById("inCor")			.value.toUpperCase().trim(),
-			"pelo": document.getElementById("inPelo")		.value.toUpperCase().trim(),
-			"raca": document.getElementById("inRaca")		.value.toUpperCase().trim(),
-			"peso": document.getElementById("inPeso")		.value.toUpperCase().trim()
+			"nome"		: document.getElementById("inNome")				.value.toUpperCase().trim(),
+			"idade"		: document.getElementById("inIdade")			.value.toUpperCase().trim(),
+			"cor"		: document.getElementById("inCor")				.value.toUpperCase().trim(),
+			"pelo"		: document.getElementById("inPelo")				.value.toUpperCase().trim(),
+			"raca"		: document.getElementById("inRaca")				.value.toUpperCase().trim(),
+			"peso"		: document.getElementById("inPeso")				.value.toUpperCase().trim(),
+			"tipoIdade" : document.getElementsByTagName("tipoIdade")	.Id,
+			"tipoPeso" 	: document.getElementsByTagName("tipoPeso")		.Id
 		}//end pet obj
 
 
@@ -34,28 +37,29 @@ function botao() {
 		// firebase.database().ref(pet.nome).child("PESO").set(pet.peso)
 
 		firebase.database().ref(pet.nome).set(	{	
-													IDADE 	: pet.idade,
+													IDADE 	: pet.idade +" " + pet.tipoIdade,
 													COR 	: pet.cor,
 													PELO 	: pet.pelo,
 													RAÇA 	: pet.raca,
-													PESO 	: pet.peso
+													PESO 	: pet.peso +" " + pet.tipoPeso
 												})
 		
 
-
+							
 		// LIMPA CAMPOS
 		// 	// document.getElementById("inNome").value = ""
 		// 	// document.getElementById("inIdade").value = ""
 		// 	// document.getElementById("inCor").value = ""
 
 		console.log(pet)
+		// location.reload()
+
 
 }//end fucn bot
 
 
 function inicioPagina(){
-	// console.log("ok")
-	
+
 	
 }//end func pagina load
 
@@ -69,7 +73,166 @@ function botaoFalse(){
 
 }
 
-	 
 
+// function botao2(){
+
+// 	var testK = document.getElementById("kilo").getAttribute("checked")
+// 	var testG = document.getElementById("grama").getAttribute("checked")
+// 	console.log(testK, testG)
 	
+// }
+
+
+//REMOVENDO DADOS
+
+//firebase.database().ref('VALOR').child('VALOR').remove()
+
+// var adaRef = firebase.database().ref('users/ada');
+// adaRef.remove()
+//   .then(function() {
+//     console.log("Remove succeeded.")
+//   })
+//   .catch(function(error) {
+//     console.log("Remove failed: " + error.message)
+//   });
+
+
+//INCLUINDO DAOS COM IDs
+
+// firebase.database().ref('VALORt').push('VALOR')
+
+// var messageListRef = firebase.database().ref('message_list');
+// var newMessageRef = messageListRef.push();
+// newMessageRef.set({
+//   'user_id': 'ada',
+//   'text': 'The Analytical Engine weaves algebraical patterns just as the Jacquard loom weaves flowers and leaves.'
+// });
+// // We've appended a new message to the message_list location.
+// var path = newMessageRef.toString();
+
+
+//SETAR VALORES
+
+// firebase.database().ref('users/ada/name').child('first').set('Ada').child('last').set('Lovelace')
+
+// var adaNameRef = firebase.database().ref('users/ada/name');
+// adaNameRef.child('first').set('Ada');
+// adaNameRef.child('last').set('Lovelace');
+
+
+//VERIFICANDO DADOS E SALVANDO 
+
+
+// firebase.database().ref(pet.nome)/*.child() para buscar um dado especifico*/.transaction(function(dados) {
+// 	if (dados === null) {
+// 		return {};//quando vazio so retorna os dados, qnd preenchido salva os dados casa nao exista
+// 		console.log('debug');
+// 	} else {
+// 		console.log('ja existe');
+// 		return; // Abort the transaction.
+// 	}
+// 	}, function(error, committed, snapshot) {
+// 	if (error) {
+// 		console.log("falha", error);
+// 	} else if (!committed) {
+// 		console.log('ja salvo.');
+// 	} else {
+// 		console.log('adicionado');
+// 	}
+// 	console.log("dados: ", snapshot.val());
+// 	})
+// }
+
+
+
+// function atualiza(){
+
+// 	let pet = {
+// 		"nome"		: document.getElementById("inNome")				.value.toUpperCase().trim(),
+// 		"idade"		: document.getElementById("inIdade")			.value.toUpperCase().trim(),
+// 		"cor"		: document.getElementById("inCor")				.value.toUpperCase().trim(),
+// 		"pelo"		: document.getElementById("inPelo")				.value.toUpperCase().trim(),
+// 		"raca"		: document.getElementById("inRaca")				.value.toUpperCase().trim(),
+// 		"peso"		: document.getElementById("inPeso")				.value.toUpperCase().trim(),
+// 		"tipoIdade" : document.getElementsByTagName("tipoIdade")	.Id,
+// 		"tipoPeso" 	: document.getElementsByTagName("tipoPeso")		.Id
+// 	}//end pet obj
+
+
+
+// 	firebase.database().ref(pet.nome).transaction(function(dados) {
+// 	if (dados === null) {
+// 		return {	
+// 			IDADE 	: pet.idade,
+// 			COR 	: pet.cor,
+// 			PELO 	: pet.pelo,
+// 			RAÇA 	: pet.raca,
+// 			PESO 	: pet.peso
+// 		}//quando vazio so retorna os dados, qnd preenchido salva os dados casa nao exista
+// 		console.log('debug');
+// 	} else {
+// 		console.log('ja existe');
+// 		return; // Abort the transaction.
+// 	}
+// 	}, function(error, committed, snapshot) {
+// 	if (error) {
+// 		console.log("falha", error);
+// 	} else if (!committed) {
+// 		console.log('ja salvo.');
+// 	} else {
+// 		console.log('adicionado');
+// 	}
+// 	console.log("dados: ", snapshot.val());
+// 	})
+// }//end func atualiza
+		
+var resultado = ""
+
+function obter(){
+	let pet = {"nome": document.getElementById("inNome").value.toUpperCase().trim()	}//end pet obj
+
+	firebase.database().ref(pet.nome).transaction(function(dados) {
+	if (dados === null) {
+		return {}//quando vazio so retorna os dados, qnd preenchido salva os dados casa nao exista
+		console.log('debug');
+	} else {
+		console.log('ja existe');
+		return; // Abort the transaction.
+	}
+	}, function(error, committed, snapshot) {
+	if (error) {
+		console.log("falha", error);
+	} else if (!committed) {
+		console.log('ja salvo.');
+	} else {
+		console.log('adicionado');
+	}
+	// console.log("dados: ", snapshot.val());
+	resultado =  snapshot.val()
+	console.log(resultado)
+	})	
+
+}//end func obter
+
+
+
+
+function recuperarDados(){
+	firebase.database().ref("MAUI").once("value",function(snapshot){
+		snapshot.foreach(
+			function(childSnapshot){
+				let test = childSnapshot.val().COR
+				// let idade = childSnapshot.val().IDADE
+				// let cor = childSnapshot.val().COR
+				// let pelo = childSnapshot.val().PELO
+				// let raca = childSnapshot.val().RACA
+				// let peso = childSnapshot.val().PESO
+
+				console.log(test)
+			}
+		)
+	})
+}//END FUCN RECUP DADOS
+console.log("ok")
+
 
---
+++ firebase.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBI87x76XQkP-1zO_OsWCQ40z0xGb6mdL0",
+    authDomain: "teste-firebase-ntp.firebaseapp.com",
+    databaseURL: "https://teste-firebase-ntp-default-rtdb.firebaseio.com",
+    projectId: "teste-firebase-ntp",
+    storageBucket: "teste-firebase-ntp.appspot.com",
+    messagingSenderId: "801768219038",
+    appId: "1:801768219038:web:803e7224fe0b848e50f68f",
+    measurementId: "G-QZWLSG5G9B"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+
+console.log("ok")
+
+// firebase.database().ref("valor").child("valor").set("valor")
+// firebase.database().ref().child("valor").remove()
+ 
+ 
+
