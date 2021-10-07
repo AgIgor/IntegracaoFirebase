da1d707334019c8912707c1e966f13de603fbe55
Your Name <you@example.com>
2021-10-06 23:00:29 -0300

    integraçao firebase

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
-    authDomain: "databasefirebasefirestore.firebaseapp.com",
-    projectId: "databasefirebasefirestore",
-    storageBucket: "databasefirebasefirestore.appspot.com",
-    messagingSenderId: "407141110375",
-    appId: "1:407141110375:web:1b1ecc00c85aabd2713ac2",
-    measurementId: "G-Y2QDD0H8NF"
-  };
+	apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
+	authDomain: "databasefirebasefirestore.firebaseapp.com",
+	projectId: "databasefirebasefirestore",
+	storageBucket: "databasefirebasefirestore.appspot.com",
+	messagingSenderId: "407141110375",
+	appId: "1:407141110375:web:1b1ecc00c85aabd2713ac2",
+	measurementId: "G-Y2QDD0H8NF"
+  }
 
   firebase.initializeApp(firebaseConfig)
 
--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
   authDomain: "databasefirebasefirestore.firebaseapp.com",
@@ -10,9 +12,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
-const dbBase = firebase.database()
-const dbStore = firebase.firestore()
-
+// const dbBase = firebase.database()
+// const dbStore = firebase.firestore()
+/*
 // dbBase.ref("pasta").child("abc").set("123")
 
 // firebase.database().ref("teste").set("valor")
@@ -63,6 +65,8 @@ function salvarFirestore(){
 });
 
 }//end salva firestore
+
+
 
 function removerFirestore(){
   console.log("removeStore")
@@ -118,17 +122,130 @@ function removerFirebase(){
 
 }//end remover firebase
 
+*/
+
+//===========================================================================================//
+
+//CRIA VALORES
+
+// var igorNameRef = firebase.database().ref('users/igor/name');
+//     igorNameRef.child('first').set('Igor');
+//     igorNameRef.child('last').set('Angeli');
 
 
 
+// UPDATE DE VALORES, SE NAO EXISTENTES SERAO CRIADOS
+
+// var igoraNameRef = firebase.database().ref('users/igor/name');
+//     igoraNameRef.update({ first: 'Igor', last: 'Angeli' });
+
+
+//REMOVE VALORES
+
+// var igorRef = firebase.database().ref('users/igor/name');
+//     igorRef.remove()
+//       .then(function() {
+//         console.log("Remove succeeded.")
+//       })
+//       .catch(function(error) {
+//         console.log("Remove failed: " + error.message)
+//       });
+
+
+//INCREMENTA VALOR, OU CRIA CASO NAO EXISTA
+
+// var igorRankRef = firebase.database().ref('valores/valor');
+//     igorRankRef.transaction(function(valorAtual) {        
+//     // console.log(valorNuvem + 1)
+//     return valorAtual += 1
+// });
+
+let valorRecebido = null
+
+//RECUPERA VALOR EM NUVEM
+
+// const dbRef = firebase.database().ref("users")
+//       dbRef.get().then((snapshot) => {
+//         if (snapshot.exists()) {
+//           // console.log(snapshot.val()) //PRINTA OU ENVIA VALOR RECEBIDO
+//           valorRecebido = snapshot.val()
+//           if(valorRecebido != null){
+//             console.log("Valor obtido",valorRecebido)
+//           }
+//         } else {
+//           console.log("Sem dados")
+//         }
+//       }).catch((error) => {
+//         console.error(error)
+//       });
+
+
+      
+
+//DETECTAR ALTERAÇOES
+
+  firebase.database().ref("users").on('value', (snapshot) => {
+        const data = snapshot.val()
+        console.log(data)
+        // updateStarCount(postElement, data)
+        valorRecebido = data
+      });
 
 
 
+  
+
+  
+  //RECUPERA KEY ID 
+
+  // var KEY = firebase.database().ref().push().key
+  // console.log(KEY)
+
+//==================================================================================//
+
+function inicioPagina() {
+  setTimeout(function(){ btnF5() }, 3000)
+
+}
+
+function btnF5(){   
+  console.log("Hello")
+  
+
+    if(valorRecebido == undefined){
+      console.log("Sem daddos")
+      return
+    }  
+
+    let itens  = Object.values(valorRecebido)
+    console.log("Array",Array.isArray(itens),itens)
 
 
 
+  let tabela = document.getElementById("tabelaLista")
+
+    itens.forEach(d => {
+      console.log(d)
+      var linha = tabela.insertRow()
+        linha.insertCell(0).innerHTML =  "#"	
+        linha.insertCell(1).innerHTML =  d.name.first
+        linha.insertCell(2).innerHTML = d.name.last
+        linha.insertCell(3).innerHTML = d.id
+    })
+
+    
+
+    // itens.forEach(myFunction)
+
+    // function myFunction(item, index, arr){
+    //   console.log("for each",item)
+
+    // }
+
+  
 
 
+}
 
 
 
@@ -154,3 +271,36 @@ function removerFirebase(){
 // let val = true
 // val ? console.log("true"):console.log("false")
  
+
+//  let lista = {
+//             "users" : {
+//               "igor" : {
+//                 "name" : {
+//                   "first" : "Igor",
+//                   "last" : "Angeli"
+//                 },
+//                 "id" : 001
+//               },
+//               "maui" : {
+//                 "name" : {
+//                   "first" : "Maui",
+//                   "last" : "Ddog"
+//                 },
+//                 "id" : 002
+//               },
+//               "mari" : {
+//                 "name" : {
+//                   "first" : "Mari",
+//                   "last" : "Cesaretti"
+//                 },
+//                 "id" : 003
+//               },
+//               "teste" : {
+//                 "name" : {
+//                   "first" : "Teste",
+//                   "last" : "123"
+//                 },
+//                 "id" : 004
+//               }
+//             }
+//           }
